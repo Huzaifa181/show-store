3458fdfdcd198645822c87de174de4375bf64776
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-07-25 00:07:01 +0500

    Basic Structure

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  TextInput,
+  useColorScheme,
+  View,
+  FlatList,
+  Text,
+  Button,
+} from 'react-native';
+import {
+  Colors,
+  DebugInstructions,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Application</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
